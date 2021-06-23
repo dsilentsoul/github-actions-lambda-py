28991d9ce9a2ae9d0b597112a93fa2e055b19578
Mainak Sanyal <sanyalmainak97@gmail.com>
2021-06-23 19:17:43 +0530

    Update pyscripts.py

--- pyscripts.py
+++ pyscripts.py
@@ -19,7 +19,7 @@ for i in l:
     if i[0]=='A' and re.search("^src/.*.py$", i[1]):
         #print(os.path.basename(i[1])[:-3])
         full_path=os.path.basename(i[1])
-        zip_file= f'zip deploy.zip {i[1]}'
+        zip_file= f'zip -j deploy.zip {i[1]}'
         subprocess.run(zip_file.split())
         function_name=os.path.splitext(full_path)[0]
         create_command= f'aws lambda create-function --function-name {function_name} --role arn:aws:iam::892244660070:role/github-actions-lambda-py --runtime python3.7 --handler {function_name}.{function_name} --zip-file fileb://deploy.zip'
